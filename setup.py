579ce63cb5fd835b9e66f7d5ca0ae6299b1fdb09
Addy-Da-Baddy <das.adriteyo@gmail.com>
2025-03-25 19:24:35 +0530

    Updated Setup files

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup, find_packages
+import os
+
+# Get list of model files to include
+model_files = [
+    os.path.join('models/default', f) 
+    for f in os.listdir('models/default')
+    if f.endswith(('.pkl', '.pth'))
+]
 
 setup(
     name="ChiefWarden",
     version="1.0",
-    packages=find_packages(),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     install_requires=[
         'pefile',
         'pandas',
@@ -16,8 +25,15 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'ChiefWarden=src.__main__:main',
+            'ChiefWarden=ChiefWarden.__main__:main',
         ],
     },
     include_package_data=True,
+    package_data={
+        'ChiefWarden': model_files,
+    },
+    # Exclude these from installation but keep in repo
+    exclude_package_data={
+        '': ['old_scripts_for_testing_and_training/*', 'LICENSE', 'README.md', 'logo.png'],
+    },
 )
